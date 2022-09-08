0e102c868aeb7ee1ba963a0e3fbc8d025541949f
s8189882 <s8189882@gmail.com>
2022-09-08 20:29:54 +0530

    Add UC2 - dequeue from the beginning

--- Queues/src/queue/using/linkedlist/QueueOperations.java
+++ Queues/src/queue/using/linkedlist/QueueOperations.java
@@ -5,12 +5,16 @@ public class QueueOperations {
 	public static void main(String[] args) {
 		Queues<Integer> queue = new Queues<Integer>(3);
 		
-		System.out.println("Creating the Queue of 56->30->70");
+		System.out.println("Dequeueing the Queue of 56->30->70");
 		queue.enqueue(56);
 		queue.printQueue();
 		queue.enqueue(30);
 		queue.printQueue();
 		queue.enqueue(70);
 		queue.printQueue();
+		while(!(queue.isEmpty())) {
+			queue.dequeue();
+			queue.printQueue();
+		}
 	}
 }
