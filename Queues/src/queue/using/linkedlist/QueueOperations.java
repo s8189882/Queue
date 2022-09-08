4adb4ecf7f6846265ea42799c5bab2917b95d39f
s8189882 <s8189882@gmail.com>
2022-09-08 20:26:56 +0530

    Add UC1 - Ability to create a Queue of 56->30->70

---
+++ Queues/src/queue/using/linkedlist/QueueOperations.java
@@ -0,0 +1,16 @@
+package queue.using.linkedlist;
+
+public class QueueOperations {
+
+	public static void main(String[] args) {
+		Queues<Integer> queue = new Queues<Integer>(3);
+		
+		System.out.println("Creating the Queue of 56->30->70");
+		queue.enqueue(56);
+		queue.printQueue();
+		queue.enqueue(30);
+		queue.printQueue();
+		queue.enqueue(70);
+		queue.printQueue();
+	}
+}
